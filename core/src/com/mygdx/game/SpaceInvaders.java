097ca8826477a5a40651a21ba4d6e2153f0d5cd2
APachon10 <Pachondestroy95@gmail.com>
2021-01-22 16:35:10 +0100

    Primer Commit

---
+++ core/src/com/mygdx/game/SpaceInvaders.java
@@ -0,0 +1,39 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class SpaceInvaders extends ApplicationAdapter {
+
+	Animation<TextureRegion> playerAnimation;
+	SpriteBatch batch;
+	Texture PlayerSheet;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		PlayerSheet = new Texture("SpaceInvaders2.png");
+
+		TextureRegion[][] tmp = TextureRegion.split(PlayerSheet);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
