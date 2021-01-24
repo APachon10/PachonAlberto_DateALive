5f2a7d19b0eb5f733c0d3b28b7ea88cb6911446a
APachon10 <Pachondestroy95@gmail.com>
2021-01-24 19:15:01 +0100

    Fondo del Sprite Sheet cambiado a negro

--- core/src/com/mygdx/game/SpaceInvaders.java
+++ core/src/com/mygdx/game/SpaceInvaders.java
@@ -38,7 +38,7 @@ public class SpaceInvaders extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stateTime += Gdx.graphics.getDeltaTime();
 
