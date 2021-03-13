edba3ef47db1c3399ef80e4115f100c36b1d2573
APachon10 <Pachondestroy95@gmail.com>
2021-03-13 10:20:04 +0100

    Reiniciando Proyecto de 0 Primer Commit

--- core/src/com/mygdx/game/SpaceInvaders.java
+++ core/src/com/mygdx/game/SpaceInvaders.java
@@ -3,69 +3,52 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class SpaceInvaders extends ApplicationAdapter {
-	private static final int FRAME_COLS = 10, FRAME_ROWS = 7;
-	//private static final int FRAME_COLS2 = 2, FRAME_ROWS2 = 1;
+import java.awt.Rectangle;
 
-	Animation<TextureRegion> playerAnimationRigth;
-	Animation<TextureRegion> playerAnimationUp;
-	Animation<TextureRegion> monsterAnimation;
-	Texture player;
-	SpriteBatch batch;
-	Texture monster;
+import sun.rmi.runtime.Log;
+
+public class SpaceInvaders extends ApplicationAdapter {
+
+	//Medidas de la pantalla
+	public static final int WIDTH = 600, HEIGHT = 400;
+
+	//Filas y Columnas del Sprite Sheet
+	public static final int FRAME_ROWS = 2, FRAME_COLS = 6;
+
+	//Elementos que usaremos
+
+	// Animations
+	Animation<TextureRegion> kurumi_walk_right
+	Animation<TextureRegion> kurumi_walk_left;
+
+	//Ortographic Camera
+	OrthographicCamera camara;
+
+	// Rectangles
+	Rectangle kurumi;
+
+	//Textures
+	Texture kurumiSprite;
+	Texture
 
 	float stateTime;
 
 	@Override
 	public void create () {
-		player = new Texture("SpaceInvaders2.png");
-		monster = new Texture("Monster.png");
-		TextureRegion[][] tmp = TextureRegion.split(player,
-				player.getWidth() / FRAME_COLS,
-				player.getHeight() / FRAME_ROWS);
 
-		// Right
-		TextureRegion[] walkRight = new TextureRegion[FRAME_COLS-2];
-		int indexRight=0;
-		for (int i=0;i<FRAME_COLS-2;i++) {
-			walkRight[indexRight++] = tmp[0][i];
-		}
-		// Up
-		TextureRegion[] walkUp = new TextureRegion[FRAME_COLS-4];
-		int indexLeft=0;
-		for (int i = 0; i < FRAME_COLS-4; i++) {
-			walkUp[indexLeft++]=tmp[1][i];
-		}
-		//Left
-		TextureRegion[] walkLeft = new TextureRegion[FRAME_COLS-4];
-		
-
-
-		playerAnimationRigth = new Animation<TextureRegion>(0.15f, walkRight);
-		playerAnimationUp = new Animation<TextureRegion>(0.15f, walkUp);
-		batch = new SpriteBatch();
-		stateTime = 0f;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stateTime += Gdx.graphics.getDeltaTime();
 
-		TextureRegion currentFrame = playerAnimationRigth.getKeyFrame(stateTime, true);
-		TextureRegion enemyFrame = monsterAnimation(stateTime,true);
-		batch.begin();
-		batch.draw(currentFrame, 270, 50);
-
-		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
